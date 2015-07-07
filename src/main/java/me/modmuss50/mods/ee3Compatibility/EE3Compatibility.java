c0329b6fac8fb9de676467893fb68ced59b22f05
modmuss50 <m123world@gmail.com>
2015-07-07 14:44:06 +0100

    Added Philosophers stone conversion

--- src/main/java/me/modmuss50/mods/ee3Compatibility/EE3Compatibility.java
+++ src/main/java/me/modmuss50/mods/ee3Compatibility/EE3Compatibility.java
@@ -1,11 +1,20 @@
 package me.modmuss50.mods.ee3Compatibility;
 
+import com.pahimar.ee3.EquivalentExchange3;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import me.modmuss50.mods.ee3Compatibility.lib.ModInfo;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
+import techreborn.init.ModItems;
 
 @Mod(name = ModInfo.MOD_NAME, modid = ModInfo.MOD_ID, version = ModInfo.MOD_VERSION)
 public class EE3Compatibility {
@@ -25,6 +34,35 @@ public class EE3Compatibility {
         if (Loader.isModLoaded("IC2")) {
             IC2EE3.postInit(event);
         }
+        MinecraftForge.EVENT_BUS.register(new EE3Compatibility());
+    }
+
+
+    @SubscribeEvent
+    public  void rightClickEvent(PlayerInteractEvent event){
+        if(event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){
+            Block block = event.world.getBlock(event.x, event.y, event.z);
+            if(event.entityPlayer.getCurrentEquippedItem() != null){
+                if(event.entityPlayer.getCurrentEquippedItem().getItem() == com.pahimar.ee3.init.ModItems.stonePhilosophers){
+                    if(event.entityPlayer.isSneaking()){
+                        if(block == Blocks.cobblestone){
+                            event.world.setBlock(event.x, event.y, event.z, Blocks.grass);
+                        }
+                        if(block == Blocks.grass){
+                            event.world.setBlock(event.x, event.y, event.z, Blocks.sand);
+                        }
+                    } else {
+                        if(block == Blocks.cobblestone){
+                            event.world.setBlock(event.x, event.y, event.z, Blocks.stone);
+                        }
+                        if(block == Blocks.stone){
+                            event.world.setBlock(event.x, event.y, event.z, Blocks.cobblestone);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
 }
